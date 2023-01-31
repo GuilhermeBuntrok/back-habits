6180f735aaadb6ffd0fed929650bbe19e0e3a3d8
GuilhermeBuntrok <guilhermebuntrock@hotmail.com>
2023-01-31 00:51:31 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,17 @@ import Fastify from "fastify";
 import cors from '@fastify/cors'
 import { appRoutes } from "./routes";
 
-
-
 const app = Fastify()
 
-const ports = 4000
-
-const port = process.env.Port || !4000
 
 app.register(cors)
 
 app.register(appRoutes)
 
 app.listen({
-  port: ports
+  port: 4000
 
 }).then(() => {
   console.log('🚀 Server ready at')
 })
+
