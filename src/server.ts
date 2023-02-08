dd619ed37c6013c0f3e2af4801e9ad09dc437492
GuilhermeBuntrok <guilhermebuntrock@hotmail.com>
2023-02-08 01:17:45 -0300

    vai

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
 import { appRoutes } from "./routes";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const PORT = process.env.PORT
 
 const app = Fastify()
 
@@ -10,7 +15,8 @@ app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-  port: 4000
+  port: 3000,
+  host: "0.0.0.0",
 
 }).then(() => {
   console.log('🚀 Server ready at')
